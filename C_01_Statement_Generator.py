537f48fa4ccc1e7da6ce414f5c143e496d1f16b1
htooe3519 <htooe3519@masseyhigh.school.nz>
2024-06-18 11:44:31 +1200

    Working statement generator

--- C_01_Statement_Generator.py
+++ C_01_Statement_Generator.py
@@ -1,8 +1,7 @@
 # functions go here
-
 def statement_generator(statement, decoration):
     print(f"\n{decoration * 5} {statement} {decoration * 5}")
 
 
-# Main routine goes heere
-statement_generator(statement="Instructions", decoration="-")
+# Main routine goes here
+statement_generator(statement="instructions", decoration= "-")
